c7a28f2e166570b2a1af22299ec5b33bb88b51e8
palaksharma1011 <palak16306@gmail.com>
2026-06-26 19:02:52 +0530

    localhost removed from origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,8 +13,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://smartfoodapp-backend.onrender.com"],
-   
+    origin: "https://smartfoodapp-backend.onrender.com",
+
     credentials: true,
   }),
 );

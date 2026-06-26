ad87e7a2d425a49e2899c5ef213f989abd6b3cfe
palaksharma1011 <palak16306@gmail.com>
2026-06-26 17:46:06 +0530

    cors origin added for new backend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://smartfoodapp-backend.onrender.com"],
+   
     credentials: true,
   }),
 );
